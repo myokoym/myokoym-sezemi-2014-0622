c886b41428e89f1eaee154082201f86dcc3a951e
Masafumi Yokoyama <myokoym@gmail.com>
2014-06-22 23:30:10 +0900

    Remove a needless file
    Because run-main.bat was replaced by recipe.bat.

--- run-main.bat
+++
@@ -1,4 +0,0 @@
-main.exe myokoym recipe-data1.txt you recipe-data2.txt she recipe-data3.txt becky recipe-data4.txt -1
-main.exe myokoym recipe-data1.txt you recipe-data2.txt she recipe-data3.txt becky recipe-data4.txt -1 5
-main.exe myokoym recipe-data1.txt you recipe-data2.txt she recipe-data3.txt becky recipe-data4.txt 2
-main.exe myokoym recipe-data1.txt you recipe-data2.txt she recipe-data3.txt becky recipe-data4.txt 4 11

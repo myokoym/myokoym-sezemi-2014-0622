7f23b7cb2c016ebf4a52f9a9a78fae3ede280e94
Masafumi Yokoyama <myokoym@gmail.com>
2014-07-09 02:16:27 +0900

    lib: add missing blank lines

--- lib/recipe.c
+++ lib/recipe.c
@@ -6,8 +6,10 @@ static int recipe_id = 1;
 Recipe *
 recipe_new() {
   Recipe *recipe;
+
   recipe = (Recipe *)malloc(sizeof(*recipe));
   recipe->id = recipe_id;
   recipe_id++;
+
   return recipe;
 }
